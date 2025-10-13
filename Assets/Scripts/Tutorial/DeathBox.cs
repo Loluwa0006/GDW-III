2c4e4ff8dd23fbc09308d6af6530401a2b4fc48b
Eman Awo <emanawosi4@gmail.com>
2025-10-13 17:19:46 -0400

    Movement Section
    Added movement section to tutorial
    
    Added death box that warps you back to starting location if you fall off

---
+++ Assets/Scripts/Tutorial/DeathBox.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.TryGetComponent(out BaseSpeaker speaker)) { return; }
+
+        speaker.healthComponent.OnEntityDeath(null, speaker.healthComponent);
+    }
+}
