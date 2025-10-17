e94bbda369a504f73db12fe1dcf2c4d03a3409dc
Eman Awo <emanawosi4@gmail.com>
2025-10-17 12:57:25 -0400

    Broken files fix
    Sahir make sure the game compiles before you commit

--- Assets/Scripts/OptionsMenuController.cs
+++ Assets/Scripts/OptionsMenuController.cs
@@ -1,32 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class OptionsMenuController : MonoBehaviour
-{
-    public static string PreviousScene;
+//public class OptionsMenuController : MonoBehaviour
+//{
+//    public static string PreviousScene;
 
-    public PauseMenuController PauseScript;
+//    public PauseMenuController PauseScript;
 
-    public MainMenuController MainScript;
+//    public MainMenuController MainScript;
 
 
-    public void ReturnToPreviousScene()
-    {
-        if (PreviousScene == null)
-        {
-            PreviousScene = "MainMenu";
-        }
-        if (PreviousScene == "SampleScene")
-        {
-            PauseScript.pauseMenuUI.SetActive(true);
-            PauseScript.OptionsMenu.SetActive(false);
-        }
-        if (PreviousScene == "MainMenu")
-        {
-            MainScript.MainMenuUI.SetActive(true);
-            MainScript.OptionsMenu.SetActive(false);
-        }
-    }
-}
+//    public void ReturnToPreviousScene()
+//    {
+//        if (PreviousScene == null)
+//        {
+//            PreviousScene = "MainMenu";
+//        }
+//        if (PreviousScene == "SampleScene")
+//        {
+//            PauseScript.pauseMenuUI.SetActive(true);
+//            PauseScript.OptionsMenu.SetActive(false);
+//        }
+//        if (PreviousScene == "MainMenu")
+//        {
+//            MainScript.MainMenuUI.SetActive(true);
+//            MainScript.OptionsMenu.SetActive(false);
+//        }
+//    }
+//}
--- Assets/Scripts/StaminaController.cs
+++ Assets/Scripts/StaminaController.cs
@@ -1,54 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
 
-public class StaminaController : MonoBehaviour
-{
-    public float maxStamina = 100f;
-    public float CurrentStamina = 100f;
-    public float GreyStamina = 100f;
+//public class StaminaController : MonoBehaviour
+//{
+//    public float maxStamina = 100f;
+//    public float CurrentStamina = 100f;
+//    public float GreyStamina = 100f;
 
-    public Image StaminaBar;
-    public Image GreyStaminaBar;
-    public InGameUI InGameUIController;
-    public GameObject GameOverUI;
+//    public Image StaminaBar;
+//    public Image GreyStaminaBar;
+//    public InGameUI InGameUIController;
+//    public GameObject GameOverUI;
 
-    //Start is called before the first frame update
-    public void Start()
-    {
-        CurrentStamina = maxStamina;
-        //StaminaBar = GetComponent<Image>();
-        StaminaBar.fillAmount = maxStamina;
-        GreyStaminaBar.fillAmount = maxStamina;
-    }
+//    //Start is called before the first frame update
+//    public void Start()
+//    {
+//        CurrentStamina = maxStamina;
+//        //StaminaBar = GetComponent<Image>();
+//        StaminaBar.fillAmount = maxStamina;
+//        GreyStaminaBar.fillAmount = maxStamina;
+//    }
 
-    //Update is called once per frame
-    void Update()
-    {
-        if (CurrentStamina <= 0)
-        {
-            GameOver();
-        }
+//    //Update is called once per frame
+//    void Update()
+//    {
+//        if (CurrentStamina <= 0)
+//        {
+//            GameOver();
+//        }
 
-        if (InGameUIController.SuddenDeathTextIsPlayed == true)
-        {
-            CurrentStamina -= Time.deltaTime * 2;
-        }
+//        if (InGameUIController.SuddenDeathTextIsPlayed == true)
+//        {
+//            CurrentStamina -= Time.deltaTime * 2;
+//        }
         
-        if (maxStamina - CurrentStamina >= 25)
-        {
-            GreyStamina -= Time.deltaTime * 2;
-        }
+//        if (maxStamina - CurrentStamina >= 25)
+//        {
+//            GreyStamina -= Time.deltaTime * 2;
+//        }
 
-        StaminaBar.fillAmount = CurrentStamina / maxStamina;
-        GreyStaminaBar.fillAmount = GreyStamina / maxStamina;
-    }
+//        StaminaBar.fillAmount = CurrentStamina / maxStamina;
+//        GreyStaminaBar.fillAmount = GreyStamina / maxStamina;
+//    }
 
-    public void GameOver()
-    {
-        Time.timeScale = 0f;
-        InGameUIController.GameUI.SetActive(false);
-        GameOverUI.SetActive(true);
-    }
-}
+//    public void GameOver()
+//    {
+//        Time.timeScale = 0f;
+//        InGameUIController.GameUI.SetActive(false);
+//        GameOverUI.SetActive(true);
+//    }
+//}
